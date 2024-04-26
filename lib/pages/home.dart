d132e53c4afe17d8f05606d69c8bf4b1064a0475
gilbert-ku <gilbert45ku@gmail.com>
2024-04-26 17:53:07 +0300

    creare a dialoge card for inputs

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toda_app/util/dialog_card.dart';
 import 'package:toda_app/util/to_do.dart';
 
 class ToDOPage extends StatefulWidget {
@@ -22,6 +23,17 @@ class _ToDOPageState extends State<ToDOPage> {
     });
   }
 
+  // create new task
+
+  void createNewTask() {
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const DialogCard();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -31,9 +43,15 @@ class _ToDOPageState extends State<ToDOPage> {
         title: const Center(child: Text("TO DO APP")),
         elevation: 0,
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: createNewTask,
+        child: const Icon(Icons.add),
+      ),
       body: ListView.builder(
         itemCount: toDoList.length,
         itemBuilder: (context, index) {
+
+          // build the list from todo list
           return ToDoCard(
             taskName: toDoList[index][0],
             taskCompleted: toDoList[index][1],
